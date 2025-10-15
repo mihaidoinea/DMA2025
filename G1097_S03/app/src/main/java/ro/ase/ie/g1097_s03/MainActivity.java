5f3f1e72fff1356a71953db5ddfa8badf544143f
Mihai Doinea <mihai.doinea@oracle.com>
2025-10-15 17:28:20 +0300

    G1097_S03

--- G1097_S03/app/src/main/java/ro/ase/ie/g1097_s03/MainActivity.java
+++ G1097_S03/app/src/main/java/ro/ase/ie/g1097_s03/MainActivity.java
@@ -1,14 +1,24 @@
 package ro.ase.ie.g1097_s03;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String MainActivityTag=getClass().getName().toLowerCase();
+
+    public MainActivity() {
+        Log.i(MainActivityTag, "MainActivity constructor.");
+
+        registerActivityLifecycleCallbacks(new MyApp());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.i(MainActivityTag, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(MainActivityTag, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(MainActivityTag, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(MainActivityTag, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(MainActivityTag, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.wtf(MainActivityTag, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(MainActivityTag, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(MainActivityTag, "onRestoreInstanceState");
     }
 }
