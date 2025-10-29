7c13413a3dc3e59c07d4b0ea9d56d3344f6ed721
Mihai Doinea <mihai.doinea@oracle.com>
2025-10-29 16:19:39 +0200

    G1091_S05

--- G1091_S04/app/src/main/java/ro/ase/ie/g1091_s04/activities/MovieActivity.java
+++ G1091_S04/app/src/main/java/ro/ase/ie/g1091_s04/activities/MovieActivity.java
@@ -1,6 +1,15 @@
 package ro.ase.ie.g1091_s04.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +18,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import ro.ase.ie.g1091_s04.R;
+import ro.ase.ie.g1091_s04.models.Movie;
 
-public class MovieActivity extends AppCompatActivity {
+public class MovieActivity extends AppCompatActivity
+//    implements View.OnClickListener
+{
+
+    private Movie movie;
+
+    private EditText etTitle;
+    private EditText etRelease;
+    private EditText etBudget;
+    private EditText etPoster;
+    private Spinner spGenre;
+    private SeekBar sbDuration;
+    private RatingBar rbRating;
+    private Switch swWatched;
+    private RadioGroup rgGuidance;
+    private Button btnMovieAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +47,65 @@ public class MovieActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initializeControls();
+        initializeEvents();
+        handleIntent();
     }
+
+/*    @Override
+    public void onClick(View view) {
+
+    }*/
+
+    private void initializeEvents() {
+        btnMovieAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                movie.setTitle(etTitle.getText().toString());
+                movie.setRating(rbRating.getRating());
+                movie.setDuration(sbDuration.getProgress());
+
+                Intent intent = new Intent();
+                //place the movie instance inside the Bundle
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+//        btnMovieAction.setOnClickListener(this);
+    }
+
+    private void initializeControls() {
+        etTitle = findViewById(R.id.etTitle);
+        etRelease = findViewById(R.id.etRelease);
+        etBudget = findViewById(R.id.etBudget);
+        etPoster = findViewById(R.id.etPoster);
+        swWatched = findViewById(R.id.swWatched);
+        spGenre = findViewById(R.id.spGenre);
+        sbDuration = findViewById(R.id.sbDuration);
+        rgGuidance = findViewById(R.id.rgApproval);
+        btnMovieAction = findViewById(R.id.btnMovieAction);
+        rbRating = findViewById(R.id.rbRating);
+    }
+
+    private void handleIntent() {
+        Intent intent = getIntent();
+        int actionCode = intent.getIntExtra("action_code", 0);
+        if(actionCode == MainActivity.ADD_MOVIE)
+        {
+            //add new movie
+            movie = new Movie();
+        }
+        else if(actionCode == MainActivity.UPDATE_MOVIE)
+        {
+            //update existing movie
+            //get the existing movie from Intent
+            //fill in the controls with movie data
+        }
+        else {
+            finish();
+        }
+    }
+
+
 }
--- G1091_S04/app/src/main/java/ro/ase/ie/g1091_s04/models/Movie.java
+++ G1091_S04/app/src/main/java/ro/ase/ie/g1091_s04/models/Movie.java
@@ -13,6 +13,42 @@ public class Movie {
     private Boolean watched; //Switch
     private String posterUrl;
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBudget(Double budget) {
+        this.budget = budget;
+    }
+
+    public void setRelease(Date release) {
+        this.release = release;
+    }
+
+    public void setGenre(GenreEnum genre) {
+        this.genre = genre;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public void setpGuidance(ParentalGuidanceEnum pGuidance) {
+        this.pGuidance = pGuidance;
+    }
+
+    public void setWatched(Boolean watched) {
+        this.watched = watched;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
     public Movie(String title, Double budget, Date release, GenreEnum genre, Float rating, Integer duration, ParentalGuidanceEnum pGuidance, Boolean watched, String posterUrl) {
         this.title = title;
         this.budget = budget;
@@ -23,6 +59,10 @@ public class Movie {
         this.pGuidance = pGuidance;
         this.watched = watched;
         this.posterUrl = posterUrl;
+    }
+
+    public Movie() {
+
     }
 
     @Override
