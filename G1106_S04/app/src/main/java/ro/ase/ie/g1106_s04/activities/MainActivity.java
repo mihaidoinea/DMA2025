df0b61e6f9d19112d1e126083a38f1fee7918ee2
Mihai Doinea <mihai.doinea@oracle.com>
2025-10-29 12:50:58 +0200

    G1106_S05

--- G1106_S04/app/src/main/java/ro/ase/ie/g1106_s04/activities/MainActivity.java
+++ G1106_S04/app/src/main/java/ro/ase/ie/g1106_s04/activities/MainActivity.java
@@ -1,8 +1,18 @@
 package ro.ase.ie.g1106_s04.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,6 +21,9 @@ import androidx.core.view.WindowInsetsCompat;
 import ro.ase.ie.g1106_s04.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ADD_MOVIE = 100;
+    private ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +35,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.add_movie_menu_item)
+        {
+            //add a new movie instance
+            Intent intent = new Intent(MainActivity.this, MovieActivity.class);
+            intent.putExtra("action_code", ADD_MOVIE);
+            launcher.launch(intent);
+        }
+        else if(item.getItemId() == R.id.about_menu_item)
+        {
+            Toast.makeText(MainActivity.this,
+                    "DMA2025 - G1106!",
+                    Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
