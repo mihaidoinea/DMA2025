6b8404982660a33f57b16a23d890b16e2948c76b
Mihai Doinea <mihai.doinea@oracle.com>
2025-11-04 11:35:26 +0200

    G1087_S06

--- G1087_S04/app/src/main/java/ro/ase/ie/g1087_s04/activities/MovieActivity.java
+++ G1087_S04/app/src/main/java/ro/ase/ie/g1087_s04/activities/MovieActivity.java
@@ -2,9 +2,12 @@ package ro.ase.ie.g1087_s04.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
@@ -18,8 +21,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import ro.ase.ie.g1087_s04.R;
+import ro.ase.ie.g1087_s04.model.GenreEnum;
 import ro.ase.ie.g1087_s04.model.Movie;
+import ro.ase.ie.g1087_s04.model.ParentalApprovalEnum;
 
 public class MovieActivity extends AppCompatActivity
 //        implements View.OnClickListener
@@ -35,8 +45,9 @@ public class MovieActivity extends AppCompatActivity
     private RadioGroup rgGuidance;
     private Switch swWatched;
     private Button btnMovieAction;
-
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
     private Movie movie;
+    private String MovieActivityTag = MovieActivity.class.getName().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,7 +99,56 @@ public class MovieActivity extends AppCompatActivity
         }
         //all other validations here
 
+        String releaseStr = etRelease.getText().toString().trim();
+        Date release = null;
+        if (!releaseStr.isEmpty()) {
+            try { release = df.parse(releaseStr); }
+            catch (ParseException e) { return ValidationResult.Error("Use date format yyyy-MM-dd.", FieldEnum.RELEASE); }
+        }
+        else
+            return ValidationResult.Error( "Release date is required.", FieldEnum.RELEASE);
+
+        String budgetStr = etBudget.getText().toString().trim();
+        double budget = 0;
+        if (!budgetStr.isEmpty()) {
+            try {
+                budget = Double.parseDouble(budgetStr);
+                if (budget < 0)
+                    return ValidationResult.Error("Budget cannot be negative.", FieldEnum.BUDGET);
+            } catch (NumberFormatException e) {
+                return ValidationResult.Error("Budget must be a number.", FieldEnum.BUDGET);
+            }
+        }
+        else
+            return ValidationResult.Error( "Budget is required.", FieldEnum.BUDGET);
+
+        String poster = etPosterUrl.getText().toString().trim();
+        if (!poster.isEmpty() && !Patterns.WEB_URL.matcher(poster).matches()) {
+            return ValidationResult.Error("Poster must be a valid URL.",FieldEnum.POSTER);
+        }
+        else if(!poster.isEmpty())
+            return ValidationResult.Error( "Poster is required.", FieldEnum.POSTER);
+
+
         movie.setTitle(title);
+        movie.setRelease(release);
+        movie.setBudget(budget);
+        movie.setPosterUrl(poster);
+        movie.setGenre(GenreEnum.valueOf(spGenre.getSelectedItem().toString()));
+        movie.setDuration(sbDuration.getProgress());
+        movie.setRating(rbRating.getRating());
+        movie.setWatched(swWatched.isChecked());
+
+        int checkedId = rgGuidance.getCheckedRadioButtonId();
+        String guidance = null;
+        if (checkedId != -1) {
+            RadioButton rb = findViewById(checkedId);
+            guidance = rb.getText().toString();
+        }
+        movie.setpApproval(ParentalApprovalEnum.valueOf(guidance));
+
+        Log.i(MovieActivityTag, movie.toString());
+
         return ValidationResult.Valid();
     }
 
