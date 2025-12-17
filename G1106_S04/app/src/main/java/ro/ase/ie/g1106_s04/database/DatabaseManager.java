6f0e36096650d6ccbd33872cfaf9156f63279303
Mihai Doinea <mihai.doinea@oracle.com>
2025-12-17 12:38:58 +0200

    G1106_S12

---
+++ G1106_S04/app/src/main/java/ro/ase/ie/g1106_s04/database/DatabaseManager.java
@@ -0,0 +1,29 @@
+package ro.ase.ie.g1106_s04.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import ro.ase.ie.g1106_s04.model.Movie;
+
+@Database(entities = {Movie.class}, version = 1)
+public abstract class DatabaseManager extends RoomDatabase{
+    private final static String databaseName = "database";
+    private static volatile DatabaseManager databaseInstance;
+
+    public static DatabaseManager getInstance(Context context){
+        if(databaseInstance == null){
+            synchronized (DatabaseManager.class){
+                if(databaseInstance == null){
+                    databaseInstance = Room.databaseBuilder(context,
+                            DatabaseManager.class, databaseName)
+                            .allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return databaseInstance;
+    }
+}
